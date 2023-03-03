967e65fb96dafb9dc045f8a183961a9429ad56c6
Dmitriy Smirnov <git@d-sm.ru>
2023-03-03 20:57:41 +0300

    eight SELECT requests completed

--- netology_pd71_db_hw4.sql
+++ netology_pd71_db_hw4.sql
@@ -21,18 +21,63 @@ SELECT m_a.name AS album_name, AVG(m_t.duration)
  GROUP BY m_a.name;
 
 -- 4. Все исполнители, которые не выпустили альбомы в 2020 году.
-SELECT m_ar.name AS artist_has_no_album_in_2020
+SELECT DISTINCT m_ar.name AS artist_has_no_album_in_2020
   FROM musical_album AS m_al
        INNER JOIN musical_album_artist AS m_al_ar
        ON m_al.id = m_al_ar.musical_album_id
-          AND m_al.year_of_release != 2020
+          AND m_al.year_of_release = 2020
        
+       INNER JOIN musical_artist AS m_ar
+       ON m_al_ar.musical_artist_id != m_ar.id;
+
+-- 5. Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+SELECT m_c.name AS music_collection_with_ray_charles
+  FROM music_collection AS m_c
+       INNER JOIN musical_track_collection AS m_t_c
+       ON m_c.id = m_t_c.music_collection_id
+
+       INNER JOIN musical_track AS m_t
+       ON m_t_c.musical_track_id = m_t.id
+
+       INNER JOIN musical_album AS m_al
+       ON m_t.musical_album_id = m_al.id
+
+       INNER JOIN musical_album_artist AS m_al_ar
+       ON m_al.id = m_al_ar.musical_album_id
+
+       INNER JOIN musical_artist AS m_ar
+       ON m_al_ar.musical_artist_id = m_ar.id
+          AND m_ar.name = 'Ray Charles';
+
+-- 6. Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+SELECT m_al.name AS multigenre_artist_album_name
+  FROM (SELECT musical_artist_id AS m_ar_id, COUNT(musical_genre_id)
+          FROM musical_artist_genre AS m_ar_g
+         GROUP BY m_ar_id
+        HAVING COUNT(musical_genre_id) > 1) AS multigenre_artist_id
+      
+       INNER JOIN musical_album_artist AS m_al_ar
+       ON m_ar_id = m_al_ar.musical_artist_id
+
+       INNER JOIN musical_album AS m_al
+       ON m_al_ar.musical_album_id = m_al.id;
+
+-- 7. Наименования треков, которые не входят в сборники.
+SELECT name AS not_in_collection_track_name
+  FROM musical_track
+ WHERE id NOT IN (SELECT DISTINCT musical_track_id
+                    FROM musical_track_collection);
+
+-- 8. Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
+SELECT DISTINCT m_ar.name AS shortest_track_artist_name
+  FROM (SELECT DISTINCT m_t.musical_album_id AS id
+          FROM musical_track AS m_t
+         WHERE m_t.duration = (SELECT MIN(duration)
+                                 FROM musical_track)) AS shortest_track_album_id
+       INNER JOIN musical_album_artist AS m_al_ar
+       ON shortest_track_album_id.id = m_al_ar.musical_album_id
+
        INNER JOIN musical_artist AS m_ar
        ON m_al_ar.musical_artist_id = m_ar.id;
 
-
--- 5. Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
--- 6. Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
--- 7. Наименования треков, которые не входят в сборники.
--- 8. Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
 -- 9. Названия альбомов, содержащих наименьшее количество треков.
